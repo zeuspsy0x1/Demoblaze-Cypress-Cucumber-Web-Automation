23cd0c06b8775b723ae744a4d7a29779cd0120d3
Zeus Vargas <zeusvargas26@gmail.com>
2022-12-30 23:45:18 -0500

    feat: added allure reports

--- cypress/support/pages/generalPages.js
+++ cypress/support/pages/generalPages.js
@@ -1,19 +1,29 @@
 let generalPagesLocators = {
     placeOrder: 'button:contains(Place Order)',
-    purchaseSweetAlert: '.sweet-alert > h2'
-
+    purchaseSweetAlert: '.sweet-alert > h2',
+    cardBody: '#tbodyid',
+    inputName: '#name',
+    inputCountry: '#country',
+    inputCity: '#city',
+    inputCard: '#card',
+    inputMonth: '#month',
+    inputYear: '#year',
+    purchaseBtn: 'button:contains(Purchase)',
 }
 
 class GeneralPages {
     getPlaceOrder() { return cy.get(generalPagesLocators.placeOrder)}
     getPurchaseSweetAlert() { return cy.get(generalPagesLocators.purchaseSweetAlert)}
+    getCardBody() { return cy.get(generalPagesLocators.cardBody)}
+    getInputName() { return cy.get(generalPagesLocators.inputName)}
+    getInputCountry() { return cy.get(generalPagesLocators.inputCountry)}
+    getInputCity() { return cy.get(generalPagesLocators.inputCity)}
+    getInputCard() { return cy.get(generalPagesLocators.inputCard)}
+    getInputMonth() { return cy.get(generalPagesLocators.inputMonth)}
+    getInputYear() { return cy.get(generalPagesLocators.inputYear)}
+    getPurchaseBtn() { return cy.get(generalPagesLocators.purchaseBtn)}
 
-   
-
-    
-    clickPlaceOrder() { return this.getPlaceOrder().click()}
-
-    
+    clickPlaceOrder() { return this.getPlaceOrder().click()}    
 }
 
 export default GeneralPages; 
